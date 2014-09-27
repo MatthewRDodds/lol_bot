a3d63ab3131c5365ffd9521a5de1af7e51a78f4f
Matt Dodds <matthewrusselldodds@gmail.com>
2014-09-27 01:40:41 -0400

    update config

--- config/config.rb
+++ config/config.rb
@@ -1,17 +1,14 @@
 require 'yaml'
 
-config = <<-YAML
+config = <<-"YAML"
 ---
-:nick: lol_bot
+:nick: 'lol_bot'
 :irc_port: 6667
-:server: irc.freenode.org
+:server: 'irc.freenode.org'
 :channels:
-- #bot_test_mbot
-- #collegeplus
-- bot_test_mbot
-- collegeplus
+- '#bot_test_mbot'
 :fifo:
-  :path: /tmp/fifo
+  :path: '/tmp/fifo'
   :mode: 0666
 :history:
   :mode: :max_messages
